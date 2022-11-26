79ecead82d80a82cec1ca726d72bd003f841e76d
Matej Zidek <zidek.matt@gmail.com>
2022-11-26 21:19:39 +0100

    web support

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:dexcom_board/services/models/glucose_event_records_dao.dart';
 import 'package:dexcom_board/services/models/station_dao.dart';
 import 'package:dexcom_board/services/save_directories.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
 
 class DatabaseService {
   DatabaseService({
@@ -15,10 +17,16 @@ class DatabaseService {
   Future<void> init() async {
     mainStore = StoreRef.main();
 
-    database =  await databaseFactoryIo.openDatabase(
-      join(_saveDirectories.dbDirPath, _dbFileName),
-      version: 1,
-    );
+    if (kIsWeb) {
+      final factory = databaseFactoryWeb;
+      database = await factory.openDatabase(_dbFileName, version: 1);
+    } else {
+      database =  await databaseFactoryIo.openDatabase(
+        join(_saveDirectories.dbDirPath, _dbFileName),
+        version: 1,
+      );
+    }
+
     await StationModelDao(dbService: this).init();
     await GlucoseEventRecordsDao(dbService: this).init();
   }
